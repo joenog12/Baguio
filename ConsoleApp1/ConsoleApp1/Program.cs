91b065ea0fe21037e8d8c4bff50b380a2948493a
unknown <Admin@CSLAB227.CSSERVER.local>
2024-02-01 19:16:33 +0800

    Activity1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+class BasicArithmetic
+{
+    static void Main()
+    {
+        int num1, num2;
+
+        Console.WriteLine("Enter the first number:");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Addition: {num1 + num2}");
+        Console.WriteLine($"Subtraction: {num1 - num2}");
+        Console.WriteLine($"Multiplication: {num1 * num2}");
+
+        if (num2 != 0)
+        {
+            double quotient = (double)num1 / num2;
+            Console.WriteLine($"Division: {quotient}");
+        }
+        else
+        {
+            Console.WriteLine("Cannot divide by zero.");
+        }
+
+        Console.ReadLine();
+    }
+}
