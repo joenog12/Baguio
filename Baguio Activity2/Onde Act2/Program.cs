2552e7993f21ba6c07e2ddcef328fdc0911cfd95
unknown <Admin@CSLAB209.CSSERVER.local>
2024-02-06 19:39:59 +0800

    COMMIT

---
+++ Baguio Activity2/Onde Act2/Program.cs
@@ -0,0 +1,56 @@
+﻿using Activity2;
+using System;
+
+namespace Activity2
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+        public void Honk(string stringVariable)
+        {
+            Console.WriteLine($"{stringVariable}: Honk!!!");
+        }
+    }
+}
+
+class Progress
+{
+    static void Main(string[] args)
+    {
+        List<Car> list = new List<Car>();
+
+        while (true)
+        {
+            Console.WriteLine("Enter new data? [Y/N]");
+
+            if (Console.ReadLine() == "N")
+            {
+                break;
+            }
+
+            Car car1 = new Car();
+
+            Console.WriteLine("Enter Car Brand = ");
+            car1.Brand = Console.ReadLine();
+
+            Console.WriteLine("Enter Car Color = ");
+            car1.Color = Console.ReadLine();
+
+            Console.WriteLine("Enter Car Price = ");
+            car1.Price = int.Parse(Console.ReadLine());
+
+            list.Add(car1);
+        }
+
+        list.ForEach(x =>
+        {
+            Console.WriteLine(x.Brand);
+            Console.WriteLine(x.Color);
+            Console.WriteLine(x.Price);
+            Console.WriteLine("=====================");
+        });
+
+    }
+}
